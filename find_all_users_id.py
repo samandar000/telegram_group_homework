dbe2817ad905675ae33e56678bd934f52f46bacf
Samandar000 <samandarnfh.2020@gmail.com>
2022-09-30 09:09:05 +0500

    03

--- find_all_users_id.py
+++ find_all_users_id.py
@@ -20,7 +20,7 @@ def find_all_users_id(data: dict)->list:
         if k!=0:
             if k not in b:
                 b.append(k)
-    return b
+    return b[:3]
+    
 d=read_data(("data/result.json"))
 
-print(find_all_users_id(d))
